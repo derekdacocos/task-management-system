88a759fed1445d9858b83c2ff67cc54fb041ee46
derekdacocos <de.dacocos24@gmail.com>
2024-12-05 04:44:20 +0800

    converted pages to have tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './views/**/*.{html,ejs}',
+    './public/css/**/*.css',
+  ],
   theme: {
     extend: {},
   },
